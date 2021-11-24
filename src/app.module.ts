07f45b9f57173659fab3dce942a9a1e969a75123
kareem444 <kareem963221@gmail.com>
2021-11-25 00:59:02 +0200

    d

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
