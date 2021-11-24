e3dda2702e7eb4b475bd3b66c614d19ab64b850d
kareem444 <kareem963221@gmail.com>
2021-11-25 01:18:12 +0200

    d

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectRepository(User)
+  private readonly userRepo: Repository<User>,) {
+
+  }
+  async create(createUserDto: CreateUserDto) {
+    const user = this.userRepo.create({ name: "kareem" })
+    return await this.userRepo.save(user);
   }
 
   findAll() {
