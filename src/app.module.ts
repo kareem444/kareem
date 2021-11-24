e5deaba3aa797934936ba14e3652bbb4fffa1ce5
kareem444 <kareem963221@gmail.com>
2021-11-25 01:23:59 +0200

    s

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,27 @@ import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
-
+  imports: [
+    TypeOrmModule.forRoot({
+      // type: 'postgres',
+      // host: 'localhost',
+      // port: 5432,
+      // username: 'postgres',
+      // password: 'kareem44',
+      // database: 'chat_server',
+      // entities: [User],
+      // synchronize: true,
+      type: 'postgres',
+      host: 'ec2-34-236-136-215.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'semubzricsdtgt',
+      password: '9c4ecc4c8b9db36b6b6e2dd022d4e62ef0e2864d2446382d51923825cec71dbe',
+      database: 'd8ta631hts0u6p',
+      entities: [User],
+      synchronize: true,
+    }),
+    // UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
